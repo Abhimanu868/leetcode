70f1ab06280af5f8e5fcc0e450218363c6c34f44
Abhimanu <abhimanu86850@gmail.com>
2026-09-25 23:26:18 +0530

    Time: 3 ms (38.14%) | Memory: 8.9 MB (35.39%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long i=1;
+        while(n>=i){
+            n-=i;
+            i++;
+        }
+        return i-1;
+    }
+};
